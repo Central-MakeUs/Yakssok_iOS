eb9e397150b167436a14b5aeab0f56cc92007f21
vvtkfkddl11 <vvtkfkddl11@naver.com>
2025-09-15 14:20:12 +0900

    [YSI-70] Fix: 회원가입 완료 시 로딩 전환 로직 수정 (튕김 현상 해결)

--- Yakssok/Yakssok/Sources/Features/Auth/Loading/LoadingFeature.swift
+++ Yakssok/Yakssok/Sources/Features/Auth/Loading/LoadingFeature.swift
@@ -45,27 +45,13 @@ struct LoadingFeature: Reducer {
                 guard !state.isRegistering else { return .none }
                 state.isRegistering = true
 
-                return .merge(
-                    // 아이콘 애니메이션
-                    .run { send in
-                        for await _ in clock.timer(interval: .milliseconds(500)) {
-                            await send(.timerTicked)
-                        }
+                return .run { send in
+                    for await _ in clock.timer(interval: .milliseconds(500)) {
+                        await send(.timerTicked)
                     }
-                    .cancellable(id: "iconTimer"),
+                }
+                .cancellable(id: "iconTimer")
 
-                    // 로딩 애니메이션만 (닉네임 업데이트 제거)
-                    .run { send in
-                        do {
-                            // 로딩 애니메이션 시간 확보
-                            try await clock.sleep(for: .seconds(2))
-                            await send(.registrationCompleted)
-                        } catch {
-                            await send(.registrationFailed(error.localizedDescription))
-                        }
-                    }
-                    .cancellable(id: "registration")
-                )
 
             case .timerTicked:
                 state.currentIconIndex = (state.currentIconIndex + 1) % 5
